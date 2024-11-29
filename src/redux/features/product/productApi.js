da35e7d9a915bf4b17ab0477cfd83af1fec3834f
idlakshan <id.lakshan21@gmail.com>
2024-11-29 08:44:55 +0530

    feat: add Product API

---
+++ src/redux/features/product/productApi.js
@@ -0,0 +1,63 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { getBaseUrl } from '../../../utils/baseURL';
+
+const productApi = createApi({
+    reducerPath: "productsApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${getBaseUrl()}/api/products`,
+        credentials: 'include',
+    }),
+    tagTypes: ["products"],
+    endpoints: (builder) => ({
+        fetchAllProducts: builder.query({
+            query: ({ category, color, minPrice, maxPrice, page = 1, limit = 10 }) => {
+                const queryParams = new URLSearchParams({
+                    category: category || '',
+                    color: color || '',
+                    minPrice: minPrice || 0,
+                    maxPrice: maxPrice || '',
+                    page: page.toString(),
+                    limit: limit.toString()
+                }).toString();
+                return `/?${queryParams}`
+            },
+            providesTags: ["products"]
+        }),
+        fetchProductById: builder.query({
+            query: (id) => ({
+                url: `/${id}`,
+                method: "GET"
+            }),
+            providesTags: (result, error, id) => [{ type: "products", id }]
+        }),
+        addProduct: builder.mutation({
+            query: (newProduct) => ({
+                url: '/create-product',
+                method: "POST",
+                body: newProduct,
+                credentials: "include"
+            }),
+            invalidatesTags: ["products"]
+        }),
+        updateProduct: builder.mutation({
+            query: ({ id, ...rest }) => ({
+                url: `/update-product/${id}`,
+                method: "PATCH",
+                body: rest,
+                credentials: "include"
+            }),
+            invalidatesTags: ["products"]
+        }),
+        deleteProduct: builder.mutation({
+            query: (id) => ({
+                url: `/${id}`,
+                method: 'DELETE',
+                credentials: 'include'
+            }),
+            invalidatesTags: (res, error, id) => [{ type: "products", id }]
+        })
+    })
+});
+
+export const {useFetchAllProductsQuery,useFetchProductByIdQuery,useAddProductMutation,useUpdateProductMutation,useDeleteProductMutation}=productApi;
+export default productApi
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './features/cart/cartSlice'
 import authApi from './features/auth/authApi';
 import authReducer from './features/auth/authSlice';
+import productApi from './features/product/productApi';
 
 const store=configureStore({
    reducer:{
     cart:cartReducer,
     [authApi.reducerPath]:authApi.reducer,
     auth:authReducer,
+    [productApi.reducerPath]:productApi.reducer,
    },
-   middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(authApi.middleware)
+   middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(authApi.middleware,productApi.middleware)
 });
 
 export default store;
